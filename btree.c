bf150103bb63284c0d982c658b610b424cf7ff32
Gustavo Alba <alba__22@outlook.com>
2020-10-01 01:10:04 -0300

    added BTree creation

--- btree.c
+++ btree.c
@@ -4,10 +4,20 @@
 
 #include "btree.h"
 
-#define MAX_KEYS (1024)
+#define MAX_KEYS (4)
 
 struct node {
   int totalKeys;
   int keys[MAX_KEYS];
   struct node *children[MAX_KEYS + 1];
 };
+
+BTree* createBTree() {
+  BTree* bTree = (BTree*) malloc(sizeof(struct node));
+  if (bTree == NULL) {
+    printf("No memory");
+    exit(1);
+  }
+  (*bTree)->totalKeys = 1;
+  return &bTree;
+}
